fc7ed61bf2466bfca7f7fda5c2265db23eaaf590
Isaiah Orville T. Aragon <isaiahorville.aragon.23@usjr.edu.ph>
2026-04-23 16:26:56 +0800

    modified:   lib/features/product/presentation/pages/product_page.dart

---
+++ lib/features/product/presentation/pages/product_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/image_section.dart';
+import '../widgets/price_section.dart';
+import '../widgets/variation_section.dart';
+import '../widgets/specs_section.dart';
+import '../widgets/delivery_section.dart';
+import '../widgets/review_section.dart';
+import '../widgets/horizontal_products.dart';
+import '../widgets/you_might_like_section.dart';
+import '../widgets/bottom_bar.dart';
+
+class ProductPage extends StatelessWidget {
+  const ProductPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // WHOLE PAGE SCROLL
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: const [
+                  ImageSection(),
+                  PriceSection(),
+                  VariationSection(),
+                  SpecsSection(),
+                  DeliverySection(),
+                  ReviewSection(),
+
+                  // ONLY horizontal section
+                  HorizontalProducts(title: "Most Popular"),
+
+                  // PART OF vertical scroll
+                  YouMightLikeSection(),
+                ],
+              ),
+            ),
+          ),
+
+          // fixed bottom
+          BottomBar(),
+        ],
+      ),
+    );
+  }
+}
